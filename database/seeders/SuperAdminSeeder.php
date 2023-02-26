d73f5649b1a2d67fb02d5e3368b15c16a5b0ed48
Kuanysh <developerandroidkz@gmail.com>
2023-02-26 23:49:05 +0500

    fix bug

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
 
 class SuperAdminSeeder extends Seeder
 {
@@ -15,6 +16,7 @@ class SuperAdminSeeder extends Seeder
      */
     public function run()
     {
+        Role::create(["name" => "Super Admin", "guard_name" => "api"]);
         $password = Hash::make("change");
         $user = User::firstOrCreate(
             ["email" => "kuanish@mail.kz"],
